03bc0435e265735c412a30726e7ba98a82a484d3
StefanoGalandrini <stefe68@gmail.com>
2023-05-22 16:51:28 +0200

    Add AppHeader content

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,7 +3,27 @@ export default {};
 </script>
 
 <template>
-	<div class="container">AppHeader</div>
+	<div class="container">
+		<img src="../assets/img/logo.webp" alt="Logo" />
+		<h1>Yu-Gi-Oh</h1>
+	</div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.container {
+	background: white;
+	height: 6vh;
+	display: flex;
+	align-items: center;
+	padding: 1rem 3rem;
+
+	img {
+		width: 100px;
+		margin-right: 3rem;
+	}
+
+	h1 {
+		display: inline-block;
+	}
+}
+</style>
