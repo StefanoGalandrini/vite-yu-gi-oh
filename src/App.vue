d02b37196c115c0ead594eff9ae83512c5ba4091
StefanoGalandrini <stefe68@gmail.com>
2023-05-22 15:07:33 +0200

    Add data in store.js visible in components

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,27 @@ import AppCardList from "./components/AppCardList.vue";
 import AppHeader from "./components/AppHeader.vue";
 import AppResult from "./components/AppResult.vue";
 import AppSearch from "./components/AppSearch.vue";
+import axios from "axios";
+import {store} from "./store";
 export default {
+	data() {
+		return {
+			cardList: [],
+			store,
+		};
+	},
+
 	components: {
 		AppHeader,
 		AppSearch,
 		AppResult,
 		AppCardList,
+	},
+
+	created() {
+		axios
+			.get("https://db.ygoprodeck.com/api/v7/cardinfo.php?num=40&offset=0")
+			.then((response) => (this.store.cardList = response.data.data));
 	},
 };
 </script>
--- src/components/AppCardList.vue
+++ src/components/AppCardList.vue
@@ -1,13 +1,27 @@
 <script>
 import AppCard from "./AppCard.vue";
+import {store} from "../store";
 export default {
+	data() {
+		return {
+			store,
+		};
+	},
+
 	components: {AppCard},
 };
 </script>
 
 <template>
+	<div v-for="card in store.cardList" :key="card.id">
+		{{ card.name }}
+		{{ card.type }}
+		{{ card.id }}
+		<img src="{{ card.card_images.image_url_small }}" alt="" />
+	</div>
 	<div class="container">
 		AppCardList
+
 		<AppCard />
 	</div>
 </template>
