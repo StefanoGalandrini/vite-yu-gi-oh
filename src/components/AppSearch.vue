30500868c6e3d8bb70461aebde847dd1ec66a035
StefanoGalandrini <stefe68@gmail.com>
2023-05-22 17:08:22 +0200

    Add AppSearch (not working yet)

--- src/components/AppSearch.vue
+++ src/components/AppSearch.vue
@@ -1,9 +1,31 @@
 <script>
-export default {};
+export default {
+	data() {
+		return {
+			selected: "",
+		};
+	},
+};
 </script>
 
 <template>
-	<div class="container">AppSearch</div>
+	<div class="container">
+		<select v-model="selected" name="filter" id="filter">
+			<option value="Alien">Alien</option>
+			<option value="Altro">Altro</option>
+		</select>
+	</div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.container {
+	width: 15%;
+	select {
+		width: 100%;
+		height: 3rem;
+		font-size: 1.2rem;
+		padding: 0.5rem 1rem;
+		margin: 1.5rem 10rem;
+	}
+}
+</style>
